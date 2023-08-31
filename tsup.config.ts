f7b8a9f4e049f40371bc31acdd686b02022528c1
unknown <2389342251@qq.com>
2023-08-31 22:53:52 +0800

    vercel配置

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup"
 
 export default defineConfig({
-  entry: ["src/main.ts"],
+  entry: ["src/index.ts"],
   outDir: "build",
   target: "es2020",
   format: ["esm"],
