5b89a1da1b9edec211ceb10e7e97b6daef3258bf
unknown <2389342251@qq.com>
2023-09-01 00:52:54 +0800

    配置3

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 const Koa = require("koa")
 const proxy = require("koa-proxy")
 const dotenv = require("dotenv")
+const axios = require("axios")
 dotenv.config()
 
 const app = new Koa()
 
-app.use(
-  proxy({
-    host: process.env.PROXY_URL,
-    match: "",
-  })
-)
+// app.use(
+//   proxy({
+//     host: process.env.PROXY_URL,
+//     match: "",
+//   })
+// )
+
+app.use(async (ctx) => {
+  try {
+    const res = await axios({
+      url: "https://api.openai.com/v1/models",
+      headers: {
+        Authorization: "Bearer " + process.env.OPENAI_KEY,
+      },
+    })
+    ctx.body = { status: "Success", data: res.data }
+  } catch (error) {
+    ctx.body = { status: "Error", message: error.message }
+  }
+})
 
 const PORT = process.env.PORT
 // 启动服务器
