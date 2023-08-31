f885110f9f193dc43d373746b5c3df22bc8550ff
unknown <2389342251@qq.com>
2023-09-01 01:35:15 +0800

    端口可修改

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(
   })
 )
 
-// 启动服务器
-app.listen(80, () => {
-  console.log(`Server is running on port 80`)
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
 })
