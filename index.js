00f27fa4d446b838893e9cb4bb7333af4b9e7046
unknown <2389342251@qq.com>
2023-09-01 01:02:17 +0800

    更换代理依赖

--- index.js
+++ index.js
@@ -1,34 +1,23 @@
 const Koa = require("koa")
-const proxy = require("koa-proxy")
+const proxy = require("koa-better-http-proxy")
 const dotenv = require("dotenv")
-const axios = require("axios")
 dotenv.config()
 
 const app = new Koa()
 
-// app.use(
-//   proxy({
-//     host: process.env.PROXY_URL,
-//     match: "",
-//   })
-// )
+app.use(
+  proxy("api.openai.com", {
+    headers: {
+      Authorization: "Bearer " + process.env.OPENAI_KEY,
+    },
+    https: true,
+    proxyReqPathResolver(ctx) {
+      return `/v1${ctx.url}`
+    },
+  })
+)
 
-app.use(async (ctx) => {
-  try {
-    const res = await axios({
-      url: "https://api.openai.com/v1/models",
-      headers: {
-        Authorization: "Bearer " + process.env.OPENAI_KEY,
-      },
-    })
-    ctx.body = { status: "Success", data: res.data }
-  } catch (error) {
-    ctx.body = { status: "Error", message: error.message }
-  }
-})
-
-const PORT = process.env.PORT
 // 启动服务器
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
+app.listen(80, () => {
+  console.log(`Server is running on port 80`)
 })
