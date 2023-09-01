217dfd5ba67b1927f99bd6f79a2a02ee85983a39
hcxhqq <2389342251@qq.com>
2023-09-01 10:27:05 +0800

    改用express

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
-const Koa = require("koa")
-const proxy = require("koa-better-http-proxy")
+// const path = require("path");
+const express = require("express")
+
+const { createProxyMiddleware } = require("http-proxy-middleware")
 const dotenv = require("dotenv")
 dotenv.config()
 
-const app = new Koa()
+const app = express()
+
+// app.use(express.static(path.join(__dirname, "public")));
 
 app.use(
-  proxy("api.openai.com", {
-    headers: {
-      Authorization: "Bearer " + process.env.OPENAI_KEY,
+  "/v1/",
+  createProxyMiddleware({
+    target: "https://api.openai.com",
+    changeOrigin: true,
+    onProxyReq: (proxyReq, req, res) => {
+      console.log(req.originalUrl)
+      proxyReq.setHeader("Authorization", `Bearer ${process.env.API_KEY}`)
     },
-    https: true,
-    proxyReqPathResolver(ctx) {
-      return `/v1${ctx.url}`
+    onProxyRes: (proxyRes, req, res) => {
+      proxyRes.headers["Access-Control-Allow-Origin"] = "*"
+      proxyRes.headers["Access-Control-Allow-Headers"] =
+        "Content-Type,Content-Length, Authorization, Accept,X-Requested-With"
     },
   })
 )
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
+  console.log(`server running on ${PORT}`)
 })
